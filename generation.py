0218a952a5144ebc09615512903bd788c0b607ab
archnemesis69 <mohamedaziz.fadhel@etudiant-enit.utm.tn>
2026-07-09 00:14:34 +0100

    cookings

--- generation.py
+++ generation.py
@@ -1,6 +1,5 @@
-from langchain_community.document_loaders
-import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_ollama import OllamaEmbeddings
 from langchain_core.output_parsers import StrOutputParser
